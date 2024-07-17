24dc26e5ff6a71449b2f94c52c424b6e95aca800
Victor Férat <victor.ferat@live.Fr>
2024-07-17 12:32:02 +0200

    Update neuxus_pipeline.py

--- Neuxus/neuxus_pipeline.py
+++ Neuxus/neuxus_pipeline.py
@@ -5,21 +5,31 @@ from neuxus.nodes import correct, io, filter, read
 from cwl_node import CWL
 from save import Save
 
-
-data_path = r'C:\Users\victor.ferat\Documents\Soraya\EEG-MRI\P05_eyes_closed_mrion.vhdr'
-
+# Read from LSL
 # signal = io.RdaReceive(rdaport=51244)
 # signal = io.LslReceive('name', 'MNE-LSL-Player', 'signal')
+
+# Read from file
+data_path = r'C:\Users\victor.ferat\Documents\Soraya\EEG-MRI\P05_eyes_closed_mrion.vhdr'
 signal = read.Reader(data_path)
 
+# GA
 signal_ga = correct.GA(signal.output, marker_input_port=signal.marker_output, start_marker='Response/R128', tr=0.8)  # 'Response/R128' is the marker of the start of every MRI volume (in case the data is read from a Brain Vision file; in case it's streamed by Brain Vision Recorder, it is 'R128')
 
+# PA
+# signal_pa = correct.PA(...)
+
+# Down-sample
 signal_ds = filter.DownSample(signal_ga.output, 20)
 
+# CWL
 signal_cwl = CWL(signal_ds.output, time_delay=21e-3, window_duration=4, overlap=0.5)
 
-signal_lsl = io.LslSend(signal_ga.output, 'raw', type='EEG')
+# Save
+signal_save = Save(signal_ga.output, filename='P05_eyes_closed_mrion-GA-raw.fif')
+signal_save = Save(signal_cwl.output, filename='P05_eyes_closed_mrion-CWL-raw.fif')
 
-signal_pa_lsl = io.LslSend(signal_cwl.output, 'cwl', type='EEG')
+# Send to LSL
+#signal_lsl = io.LslSend(signal_ga.output, 'raw', type='EEG')
+#signal_pa_lsl = io.LslSend(signal_cwl.output, 'cwl', type='EEG')
 
-signal_save = Save(signal_cwl.output, filename='fafa')
